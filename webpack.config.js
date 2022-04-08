e81a682b29adaebf37003405d0bb1e67eee20350
GeorgeSemenov <zhora.sem@yandex.ru>
2022-04-08 06:37:16 +0300

    end of this project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -10,6 +11,7 @@ module.exports = {
         filename: '[name].bundle.js',
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'webpack Boilerplate',
             template: path.resolve(__dirname, './src/template.html'), // шаблон
