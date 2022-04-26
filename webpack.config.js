1c347e1581e67f2ef0a970620ff03c1babb920db
GeorgeSemenov <zhora.sem@yandex.ru>
2022-04-26 09:36:27 +0300

    finally it's over with some pug files

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,15 @@ module.exports = function(env, argv){
                 assetModuleFilename: "assets/[hash][ext][query]",//Все ресурсы помеченные как type:asset будут сохраняться теперь в папку assets в папке dist
                 clean: true,//Теперь при создании папка dist вначале будет удаляться, а затем будет наполняться.
             },
+            devtool: 'source-map',//Будет создаваться сорсмап, это значит, что когда ты вызовешь инструмент разработчика в браузере и посмотрищь на свойства элемента, то там будет прописанно, в каком конкретно файле прописанно это свойства, если не указать сорсмап, то все свйоства будут храниться в inline, т.к. в режиме разработки мы сохраняем их туда.
             plugins: [
                 new extractCss({
                     filename: '[name].[contenthash].css',//Теперь генерируемый файл стилей будет иметь в названии хэш, чтобы не подвергаться кэшированию браузером клиента.
                 }),
                 new HtmlWebpackPlugin({
                     title: 'webpack Boilerplate',
-                    template: path.resolve(__dirname, './src/template.html'), // шаблон
+                    // template: path.resolve(__dirname, './src/template.html'), // шаблон
+                    template: path.resolve(__dirname, './src/index.pug'), // шаблон
                     filename: 'index.html', // название выходного файла
                 }),
             ],
@@ -40,6 +42,11 @@ module.exports = function(env, argv){
                     {
                         test: /\.jpg$/i,
                         type: 'asset/resource'
+                    },
+                    {
+                        test: /\.pug$/,
+                        loader: 'pug-loader',
+                        exclude: /(node_modules|bower_components)/,//Не удалось найти, что значит эта строка, но наверное, она исключает какой-то набор файлов, которые не нужно будет подгружать через pug-loader
                     }
                 ]
             }
--- webpackConfig/devserver.js
+++ webpackConfig/devserver.js
@@ -2,21 +2,12 @@ const path = require('path')
 module.exports = function(){
   return{
     devServer: {
-<<<<<<< HEAD
-      watchFiles: ["src/*.html"],//Если не вписать эту строку, то devserver будет следить только за обновлениями в файлах js и scss(если они import'ы к ним прописаны в js)
       static: {
-        directory: path.join(__dirname, 'dist'),
+        directory: './src',
+        watch: true,
       },
       compress: true,
       port: 9000,
-=======
-        watchFiles: ["*.html"],//Если не вписать эту строку, то devserver будет следить только за обновлениями в файлах js и scss(если они import'ы к ним прописаны в js)
-        static: {
-          directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
->>>>>>> 52e2d1aa488d3bb9b8445a36acf983afe406d55b
     }
   }
 }
