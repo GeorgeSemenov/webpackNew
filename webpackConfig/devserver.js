33619481a0890811ed317612627580457748b274
GeorgeSemenov <zhora.sem@yandex.ru>
2022-04-26 06:12:31 +0300

    add прочти меня

--- webpackConfig/devserver.js
+++ webpackConfig/devserver.js
@@ -2,11 +2,12 @@ const path = require('path')
 module.exports = function(){
   return{
     devServer: {
-        static: {
-          directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
+      watchFiles: ["src/*.html"],//Если не вписать эту строку, то devserver будет следить только за обновлениями в файлах js и scss(если они import'ы к ним прописаны в js)
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
     }
   }
 }
